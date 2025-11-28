25bee81f71a03af1eaaf94f57578f26d061e6826
Cristiano <cristiano.rotunno@gmail.com>
2025-11-28 11:50:03 +0100

    .

--- main.cpp
+++ main.cpp
@@ -5,19 +5,18 @@
 #include <vector>
 
 int main() {
-    Dataset dataset = Dataset::from_csv("data/mnist_train.csv", {"ALL"}, "label");
+    Dataset dataset = Dataset::from_csv("data/iris.csv", {"ALL"}, "species");
 
     Network network(
         {
-            Layer(784, 128, Activation::RELU),
-            Layer(128, 32, Activation::RELU),
-            Layer(32, 10, Activation::SOFTMAX)
+            Layer(4, 4, Activation::RELU),
+            Layer(4, 3, Activation::SOFTMAX)
         },
         InitType::He,
         0.01
     );
     
-    network.train(dataset, 10);
+    network.train(dataset, 1000);
     
     return 0;
 }
